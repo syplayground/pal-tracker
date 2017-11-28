96db49feb4815bcaaf19ab513ce8a870c2706dc0
sudheer y <sudheer.yakkala@gmail.com>
2017-11-28 09:05:45 -0800

    Concourse files

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,42 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController{
+
+    private final String port;
+    private final String memory_limit;
+    private final String cf_instance_index;
+    private final String cf_instance_addr;
+
+
+
+    public EnvController(@Value("${PORT:NOT SET}") String port,
+                         @Value("${MEMORY_LIMIT:NOT SET}") String memory_limit,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}") String cf_instance_index,
+                         @Value("${CF_INSTANCE_ADDR:NOT SET}") String cf_instance_addr){
+        this.port = port;
+        this.memory_limit = memory_limit;
+        this.cf_instance_addr = cf_instance_addr;
+        this.cf_instance_index = cf_instance_index;
+
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv(){
+        Map<String, String> env = new HashMap<>();
+        env.put("PORT",port);
+        env.put("MEMORY_LIMIT",memory_limit);
+        env.put("CF_INSTANCE_ADDR",cf_instance_addr);
+        env.put("CF_INSTANCE_INDEX",cf_instance_index);
+
+        return env;
+    }
+
+}
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,12 +2,19 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
 
+    String welcomeMessage ;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String msg){
+        welcomeMessage = msg;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomeMessage;
     }
 }
